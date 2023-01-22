1c37a05fc13bc4907955ee323a4b9f32004b16ae
Lewis Lovett <llovett7575@gmail.com>
2023-01-22 22:00:05 +0000

    Create Vehicle abstract class

---
+++ src/main/java/com/nology/parking/vehicles/Vehicle.java
@@ -0,0 +1,48 @@
+package com.nology.parking.vehicles;
+
+public abstract class Vehicle implements Comparable<Vehicle> {
+
+    protected int id;
+    protected int requiredNumberOfCompactSpots;
+    protected int requiredNumberOfRegularSpots;
+    protected int requiredNumberOfLargeSpots;
+
+    public Vehicle(int id, int requiredNumberOfCompactSpots, int requiredNumberOfRegularSpots, int requiredNumberOfLargeSpots) {
+        this.id = id;
+        this.requiredNumberOfCompactSpots = requiredNumberOfCompactSpots;
+        this.requiredNumberOfRegularSpots = requiredNumberOfRegularSpots;
+        this.requiredNumberOfLargeSpots = requiredNumberOfLargeSpots;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRequiredNumberOfCompactSpots() {
+        return requiredNumberOfCompactSpots;
+    }
+
+    public void setRequiredNumberOfCompactSpots(int requiredNumberOfCompactSpots) {
+        this.requiredNumberOfCompactSpots = requiredNumberOfCompactSpots;
+    }
+
+    public int getRequiredNumberOfRegularSpots() {
+        return requiredNumberOfRegularSpots;
+    }
+
+    public void setRequiredNumberOfRegularSpots(int requiredNumberOfRegularSpots) {
+        this.requiredNumberOfRegularSpots = requiredNumberOfRegularSpots;
+    }
+
+    public int getRequiredNumberOfLargeSpots() {
+        return requiredNumberOfLargeSpots;
+    }
+
+    public void setRequiredNumberOfLargeSpots(int requiredNumberOfLargeSpots) {
+        this.requiredNumberOfLargeSpots = requiredNumberOfLargeSpots;
+    }
+}
